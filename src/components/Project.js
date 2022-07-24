f0e89b96e7a4eb5907a92eb120fd7d93aafe5b51
YonisHussein <yonismhussein@gmail.com>
2022-07-23 23:24:25 -0400

    project.js had been added

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Project() {
+    return (
+        <div id="project">
+            <h2>Project</h2>
+            <a href=""></a>
+        </div>
+    )
+}
+
+export default Project;
